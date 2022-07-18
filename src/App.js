a9ad6b75508af4d5f412aee913bf61d91ce3677a
den1aka1aki <lavrov.den@gmail.com>
2022-07-18 06:54:39 +0200

    new_lesson_pagination

--- src/App.js
+++ src/App.js
@@ -4,10 +4,24 @@ import "bootstrap/dist/css/bootstrap.css";
 import SearchStatus from "./components/searchStatus";
 import api from "./api";
 import Users from "./components/users";
+import Pagination from "./components/pagination";
+import {paginate} from "./app/utils/paginate";
 
 
 const App = () => {
   const [users, setUsers] = useState(api.users.fetchAll());
+  const count = users.length;
+  const pageSize = 4;
+  const [currentPage, setCurrentPage] = useState(1)
+  const handlePageChange = (pageIndex) =>{
+
+    setCurrentPage(pageIndex);
+  }
+
+  const userCrop = paginate(users, currentPage, pageSize);
+
+
+
   const handleBookmark = (userBookmark) => {
     setUsers((prevState) =>
       prevState.map((user) => {
@@ -29,7 +43,7 @@ const App = () => {
   return (
     <>
       <SearchStatus lenght={users.length} />
-      {users.length > 0 ? (
+      {count> 0 ? (
         <div>
           <table className="table">
             <thead>
@@ -44,9 +58,11 @@ const App = () => {
               </tr>
             </thead>
             <tbody>
-              <Users users={users} onDelete={handleDelete} onBookmark={handleBookmark} />
+              <Users users={userCrop} onDelete={handleDelete} onBookmark={handleBookmark} />
             </tbody>
           </table>
+          <Pagination itemsCount = {count} pageSize = {pageSize} currentPage = {currentPage} onPageChange = {handlePageChange}/>
+
         </div>
       ) : null}
     </>
