b6bf9eb8987c716136d0252b35864a55f2691eae
den1aka1aki <lavrov.den@gmail.com>
2022-08-24 06:35:20 +0200

    new_lesson

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,16 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TextField from '../components/textField';
+import { validator } from '../utils/validator';
 
 const Login = () => {
     const [data, setData] = useState({ email: '', password: '' });
+    const [errors, setErrors] = useState({});
     const handleChange = ({ target }) => {
         setData((prevState) => ({
             ...prevState,
             [target.name]: target.value
         }));
     };
+    const validatorConfig = {
+        email: {
+            isRequired:
+                    {
+                        message: 'Электронная почта обязательна для заполнения'
+                    },
+            isEmail: {
+                message: 'Email введен не верно'
+            }
+        },
+        password: {
+            isRequired:
+                    {
+                        message: 'Пароль обязательна для заполнения'
+                    }
+        }
+    };
+    useEffect(() => {
+        validate();
+    }, [data]);
+    const validate = () => {
+        const errors = validator(data, validatorConfig);
+        setErrors(errors);
+        return Object.keys(errors).length === 0 || false;
+    };
     const handleSubmit = (e) => {
         e.preventDefault();
+        const isValid = validate();
+        if (!isValid) return;
+        console.log(data);
     };
     return (
         <form onSubmit={handleSubmit}>
@@ -19,6 +49,7 @@ const Login = () => {
                 name = 'email'
                 value = {data.email}
                 onChange = {handleChange}
+                error = {errors.email}
             />
             <TextField
                 label = 'Пароль'
@@ -26,6 +57,7 @@ const Login = () => {
                 name = 'password'
                 value = {data.password}
                 onChange = {handleChange}
+                error = {errors.password}
             />
             <button>Submit</button>
         </form>
